3e3efab1bd3c15c0cbbb2ceecabc06a45a4120cd
SaitoYuuki48 <115055003+SaitoYuuki48@users.noreply.github.com>
2024-09-06 16:50:04 +0900

    タイトルシーン中身追加

--- DirectXGame/TitleScene.h
+++ DirectXGame/TitleScene.h
@@ -1,2 +1,46 @@
 #pragma once
-class TitleScene {};
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "Model.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+class TitleScene {
+public:
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	TitleScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~TitleScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+
+	// ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	// ビュープロジェクション
+	ViewProjection viewProjection_;
+};
